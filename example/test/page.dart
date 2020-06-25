443e7f7dd39350c8a905f81d55a593e27238485d
zeeshux7860 <zeeshux7860.zx@gmail.com>
2020-06-25 21:39:46 +0530

    sixtyfive commit

--- example/test/page.dart
+++ example/test/page.dart
@@ -1,1 +1,12 @@
+import 'package:material_x/laravel.dart';
 
+void main() {
+  print(laraveRequiredValidation(list: li));
+}
+
+var li = <String>[
+  "1".toLongText(),
+  "2".toLongText(),
+  "3".toLongText(),
+  "4".toLongText(),
+];
