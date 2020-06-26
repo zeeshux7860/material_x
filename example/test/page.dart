5e627bc8462457c9334cd1c429253f0061148938
zeeshux7860 <zeeshux7860.zx@gmail.com>
2020-06-26 18:25:01 +0530

    first commit

--- example/test/page.dart
+++ example/test/page.dart
@@ -1,18 +1,23 @@
 import 'package:material_x/laravel.dart';
 
 void main() {
-  f();
+  var ok = LaravelGetData(tableName: "ok")
+      .where('id', '>', 'user_id'.toVariable())
+      .where('id', '>', 'user_id'.toVariable())
+      .paginate()
+      .laravelDone();
+  print(ok.toStoreVariable(variablename: "ok"));
 }
 
 void f() {
- var o =  [
+  var o = [
     "zeeshan".toStoreVariable(variablename: "name"),
-  "zeeshan".toStoreVariable(variablename: "names"),
-
-   Condition('name'.toVariable(), '==', 'names'.toVariable(),
-          falseValue: "false".toBoolReturn(), trueValue: "true".toBoolReturn())
-      .check()
-  ].toFuntions("ok",request: true);
+    "zeeshan".toStoreVariable(variablename: "names"),
+    Condition('name'.toVariable(), '==', 'names'.toVariable(),
+            falseValue: "false".toBoolReturn(),
+            trueValue: "true".toBoolReturn())
+        .check()
+  ].toFuntions("ok", request: true);
 
   print(o);
 }
